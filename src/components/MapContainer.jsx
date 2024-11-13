f92628917aad84bb2686f3a7eb7108cc2be967a4
Julian Moreno <juliancastillo204@gmail.com>
2024-11-12 18:08:21 -0600

    arreglando bug de market en map

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,5 +1,14 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Crear un ícono personalizado utilizando tu imagen local
+const customIcon = new L.Icon({
+  iconUrl: '/src/icons/map.svg',  // Ajusta la ruta a la de tu proyecto
+  iconSize: [40, 50],  // Ajusta el tamaño según necesites
+  iconAnchor: [20, 30],  // Ajusta el punto de anclaje (centro inferior del ícono)
+  popupAnchor: [0, -30],  // Posición del popup respecto al ícono
+});
 
 export default function MapContainerComponent() {
   const position = [13.7083973, -89.2061803];
@@ -10,7 +19,7 @@ export default function MapContainerComponent() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
+      <Marker position={position} icon={customIcon}>
         <Popup>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, facilis!
         </Popup>
